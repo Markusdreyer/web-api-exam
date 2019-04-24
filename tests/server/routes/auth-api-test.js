6c7cffe2e587702b0408cd92e94ffa672d477365
Markus Dreyer <markus.a.dreyer@gmail.com>
2019-04-24 11:05:53 +0200

    added authentication tests (45.21%)

--- tests/server/routes/auth-api-test.js
+++ tests/server/routes/auth-api-test.js
@@ -16,7 +16,6 @@ test("Test fail login", async () => {
     expect(response.statusCode).toBe(401);
 });
 
-
 test("Test fail access data of non-existent user", async () => {
 
     const response = await request(app)
@@ -25,37 +24,25 @@ test("Test fail access data of non-existent user", async () => {
     expect(response.statusCode).toBe(401);
 });
 
-
-test("Test create user, but fail get data", async () => {
-
-    const userId = "foo"
-
-    let response = await request(app)
-        .post('/api/login')
-        .send({ userId: 'foo', password: "bar" })
-        .set('Content-Type', 'application/json');
-
-    expect(response.statusCode).toBe(204);
-
-
-    //no use of cookies here, so auth fails
-    response = await request(app)
-        .get('/api/user');
-
-    expect(response.statusCode).toBe(401);
-});
-
 test("Test login after logout", async () => {
 
-    const userId = "foo"
+    const userId = 'foo_' + (counter++);
 
+    //use same cookie jar for the HTTP requests
     const agent = request.agent(app);
 
-    let response = await request(app)
-        .post('/api/login')
-        .send({ userId: 'foo', password: "bar" })
+    //create user
+    let response = await agent
+        .post('/api/signup')
+        .send({ userId, password: "bar" })
         .set('Content-Type', 'application/json');
-    expect(response.statusCode).toBe(204);
+    expect(response.statusCode).toBe(201);
+
+
+    //can get info
+    response = await agent.get('/api/user');
+    expect(response.statusCode).toBe(200);
+
 
     //now logout
     response = await agent.post('/api/logout');
